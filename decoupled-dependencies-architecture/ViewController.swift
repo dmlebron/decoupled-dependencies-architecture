79c4212a9a6eb6af4dd9d9666a51cf4c1d8a7af2
David <davamale@gmail.com>
2018-04-20 11:47:24 -0400

    initial state

--- decoupled-dependencies-architecture/ViewController.swift
+++ decoupled-dependencies-architecture/ViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var searchStackView: UIStackView!
-    @IBOutlet weak var artistNameText: UITextField!
+    @IBOutlet weak var searchText: UITextField!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     private var resultsArray = [NSDictionary]() {
         didSet {
+            artistNameLabel.text = resultsArray.first!["artistName"] as? String
             tableView.reloadData()
         }
     }
@@ -26,16 +27,46 @@ class ViewController: UIViewController {
         
         tableView.tableFooterView = UIView()
         
-        ApiService.get(withResultType: Type.album, forArtist: "Mago de oz", completion: { [unowned self] (response)  in
+        searchButton.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+    }
+}
+
+// MARK: Selectors
+@objc extension ViewController {
+    func searchButtonTapped() {
+        guard isArtistNameValid() else {
+            return self.handleNoResponse()
+        }
+        
+        self.search(forArtist: searchText.text!)
+    }
+}
+
+// MARK: Private
+private extension ViewController {
+    private func handleNoResponse() {
+        self.artistNameLabel.text = "Nothing Found"
+    }
+    
+    private func search(forArtist artist: String) {
+        ApiService.get(withResultType: Type.album, forArtist: artist, completion: { [unowned self] (response)  in
             guard let response = response else {
-                self.artistNameLabel.text = "No Artist"
+                self.handleNoResponse()
                 return
             }
             self.resultsArray = response
         })
     }
+    
+    private func isArtistNameValid() -> Bool {
+        guard let artist = searchText.text, !artist.isEmpty else {
+            return false
+        }
+        return true
+    }
 }
 
+// MARK: UITableViewDataSource
 extension ViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -46,13 +77,22 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = resultsArray[indexPath.row]["collectionName"] as? String
+        
+        return cell
     }
 }
 
+// MARK: UITextFieldDelegate
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        searchButton.resignFirstResponder()
+        searchText.resignFirstResponder()
+        guard isArtistNameValid() else {
+            return false
+        }
+        search(forArtist: searchText.text!)
         return true
     }
 }
