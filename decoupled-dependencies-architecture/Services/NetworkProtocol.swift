1e9c47bb27241d42181f28e9fbf50c9ce09aaf36
David <davamale@gmail.com>
2018-04-21 12:54:30 -0400

    protocols

---
+++ decoupled-dependencies-architecture/Services/NetworkProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkProtocol.swift
+//  decoupled-dependencies-architecture
+//
+//  Created by David Martinez-Lebron on 4/21/18.
+//  Copyright © 2018 davaur. All rights reserved.
+//
+
+import Foundation
