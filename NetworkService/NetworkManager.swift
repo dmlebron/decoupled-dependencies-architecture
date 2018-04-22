157bd4c5ab425533065a13ddfa87b12e1e79488a
David <davamale@gmail.com>
2018-04-22 00:20:10 -0400

    added alamofire in networkmanager

--- NetworkService/NetworkManager.swift
+++ NetworkService/NetworkManager.swift
@@ -9,24 +9,10 @@ import Foundation
 import Alamofire
 
 class NetworkManager: Networking {
-
+    
     static func get(fromUrl url: URL, completion: @escaping (Any?) -> ()) {
-
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data, error == nil else {
-                return completion(nil)
-            }
-            do {
-                let deserialized = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-                DispatchQueue.main.async {
-                    completion(deserialized)
-                }
-
-            } catch {
-                DispatchQueue.main.async {
-                    completion(nil)
-                }
-            }
-            }.resume()
+        Alamofire.request(url.absoluteString).responseJSON { (response) in
+            print(response)
+        }
     }
 }
